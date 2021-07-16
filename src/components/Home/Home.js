e92858061e1c8c9e2c1da0b9f65149fc7ff9f56f
nadirbashir <1.nadirbashir@gmail.com>
2021-07-16 17:24:32 +0500

    lifting state up to home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,8 +2,98 @@ import React, { useState } from "react";
 import classes from "./Home.module.css";
 import Intro from "./Intro";
 import Quiz from "./Quiz";
+
+const arrayOfQuiz = [
+  {
+    question: "Whose standard is ES-262?",
+    choices: ["HTML", "Ruby", "Kotlin", "JavaScript"],
+    correctChoice: "JavaScript",
+  },
+  {
+    question: "What does &nbsp; does in an HTML page?",
+    choices: [
+      "Adds space(s)",
+      "Adds link break",
+      "Adds comma",
+      "Does Nothing",
+    ],
+    correctChoice: "Adds space(s)",
+  },
+  {
+    question: "What is not a value of the property 'position'?",
+    choices: ["absolute", "fixed", "jiggly", "relative"],
+    correctChoice: "jiggly",
+  },
+  {
+    question: "What is best for creating responsive sites?",
+    choices: ["Float", "Flexbox", "Margin", "Padding"],
+    correctChoice: "Flexbox",
+  },
+  {
+    question: "What is called 'sibling selector' in CSS?",
+    choices: [".", "+", ">", "~"],
+    correctChoice: "+",
+  },
+  {
+    question:
+      "If a local and global variable have the same name, which one will be used inside the function?",
+    choices: ["Local", "Global", "Both", "None"],
+    correctChoice: "Local",
+  },
+  {
+    question: "What are arrays in JavaScript??",
+    choices: ["Arrays", "Square Brackets", "Containers", "Objects"],
+    correctChoice: "Objects",
+  },
+  {
+    question: "What does ECMA stand for?",
+    choices: [
+      "Every Convenience Made Available",
+      "European Computer Manufacturers Association",
+      "England China Malaysia America?",
+      "It means nothing",
+    ],
+    correctChoice: "European Computer Manufacturers Association",
+  },
+  {
+    question: "What is localStorage() used for?",
+    choices: [
+      "To store data in system memory",
+      "To store data in browser",
+      "Same as sessionStorage()",
+      "To store data in browser without an expiry time",
+    ],
+    correctChoice: "To store data in browser without an expiry time",
+  },
+  {
+    question: "Can clearInterval() be used inside setInterval()?",
+    choices: ["Yes", "No", "Sometimes", "Who cares?"],
+    correctChoice: "Yes",
+  },
+];
 const Home = (props) => {
-  const [isStart, setIsStart] = useState()
+  const [isStart, setIsStart] = useState();
+
+  const [question, setQuestion] = useState(0);
+  const [score,setScore] = useState(0)
+  console.log(score);
+  const questionSubmitHandler = (event) =>{
+    event.preventDefault();
+    let form = document.querySelector("form");
+    let data = new FormData(form);
+    let userAnswer = "";
+    for (const entry of data) {
+        userAnswer = entry[1];
+  };
+  if(userAnswer === arrayOfQuiz[question].correctChoice){
+    setScore(score + 10);
+  }
+  if(question === arrayOfQuiz.length-1){
+      setQuestion(0);
+    }else{
+        setQuestion(question+1);
+    }
+}
 
   const onStart = () =>{
     localStorage.setItem("quizStart","1");
@@ -13,7 +103,7 @@ const Home = (props) => {
   return (
     <div className={classes.home}>
       {!isStart && <Intro onStart={onStart} />}
-    {isStart && <Quiz /> }
+    {isStart && <Quiz question={question} onQuestionSubmit={questionSubmitHandler} arrayOfQuiz={arrayOfQuiz}/> }
     </div>
   );
 };
--- src/components/Home/Quiz.js
+++ src/components/Home/Quiz.js
@@ -1,131 +1,51 @@
 import { useState } from "react";
 import { Card } from "../UI/Card/Card";
+import classes from './Quiz.module.css'
 
-const Quiz = () => {
-  const [question, setQuestion] = useState(0);
-  const [score,setScore] = useState(0)
-  let form = document.querySelector("form");
-  let arrayOfQuiz = [
-    {
-      question: "Whose standard is ES-262?",
-      choices: ["HTML", "Ruby", "Kotlin", "JavaScript"],
-      correctChoice: "JavaScript",
-    },
-    {
-      question: "What does &nbsp; does in an HTML page?",
-      choices: [
-        "Adds space(s)",
-        "Adds link break",
-        "Adds comma",
-        "Does Nothing",
-      ],
-      correctChoice: "Adds space(s)",
-    },
-    {
-      question: "What is not a value of the property 'position'?",
-      choices: ["absolute", "fixed", "jiggly", "relative"],
-      correctChoice: "jiggly",
-    },
-    {
-      question: "What is best for creating responsive sites?",
-      choices: ["Float", "Flexbox", "Margin", "Padding"],
-      correctChoice: "Flexbox",
-    },
-    {
-      question: "What is called 'sibling selector' in CSS?",
-      choices: [".", "+", ">", "~"],
-      correctChoice: "+",
-    },
-    {
-      question:
-        "If a local and global variable have the same name, which one will be used inside the function?",
-      choices: ["Local", "Global", "Both", "None"],
-      correctChoice: "Local",
-    },
-    {
-      question: "What are arrays in JavaScript??",
-      choices: ["Arrays", "Square Brackets", "Containers", "Objects"],
-      correctChoice: "Objects",
-    },
-    {
-      question: "What does ECMA stand for?",
-      choices: [
-        "Every Convenience Made Available",
-        "European Computer Manufacturers Association",
-        "England China Malaysia America?",
-        "It means nothing",
-      ],
-      correctChoice: "European Computer Manufacturers Association",
-    },
-    {
-      question: "What is localStorage() used for?",
-      choices: [
-        "To store data in system memory",
-        "To store data in browser",
-        "Same as sessionStorage()",
-        "To store data in browser without an expiry time",
-      ],
-      correctChoice: "To store data in browser without an expiry time",
-    },
-    {
-      question: "Can clearInterval() be used inside setInterval()?",
-      choices: ["Yes", "No", "Sometimes", "Who cares?"],
-      correctChoice: "Yes",
-    },
-  ];
 
-  const questionSubmitHandler = (event) =>{
-    event.preventDefault();
-    let data = new FormData(form);
-    let userAnswer = "";
-    for (const entry of data) {
-        userAnswer = entry[1];
-  };
-  console.log(score);
-  if(userAnswer === arrayOfQuiz[question].correctChoice){
-      setScore(score + 10);
-  }
-  if(question === arrayOfQuiz.length){
-      setQuestion(0);
-    }else{
-        setQuestion(question+1);
-    }
-}
+const Quiz = (props) => {
+  
   return (
-    <Card>
-      <form onSubmit={questionSubmitHandler}>
-        <h4>{`Q${question+1}) ${arrayOfQuiz[question].question}`}</h4>
-        <div>
+    <Card className={classes.quiz}>
+      <form onSubmit={props.onQuestionSubmit}>
+        <h4>{`Q${props.question+1}) ${props.arrayOfQuiz[props.question].question}`}</h4>
+        <div className={classes['choice-box']}>
           <input
             type="radio"
             name="answer"
             id="choice0"
-            value={arrayOfQuiz[question].choices[0]}
+            value={props.arrayOfQuiz[props.question].choices[0]}
           />
-          <label htmlFor="choice0">{arrayOfQuiz[question].choices[0]}</label>
+          <label htmlFor="choice0">{props.arrayOfQuiz[props.question].choices[0]}</label>
+          <br />
           <input
             type="radio"
             name="answer"
             id="choice1"
-            value={arrayOfQuiz[question].choices[1]}
+            value={props.arrayOfQuiz[props.question].choices[1]}
           />
-          <label htmlFor="choice1">{arrayOfQuiz[question].choices[1]}</label>
+          <label htmlFor="choice1">{props.arrayOfQuiz[props.question].choices[1]}</label>
+          <br />
           <input
             type="radio"
             name="answer"
             id="choice2"
-            value={arrayOfQuiz[question].choices[2]}
+            value={props.arrayOfQuiz[props.question].choices[2]}
           />
-          <label htmlFor="choice2">{arrayOfQuiz[question].choices[2]}</label>
+          <label htmlFor="choice2">{props.arrayOfQuiz[props.question].choices[2]}</label>
+          <br />
           <input
             type="radio"
             name="answer"
             id="choice3"
-            value={arrayOfQuiz[question].choices[3]}
+            value={props.arrayOfQuiz[props.question].choices[3]}
           />
-          <label htmlFor="choice3">{arrayOfQuiz[question].choices[3]}</label>
+          <label htmlFor="choice3">{props.arrayOfQuiz[props.question].choices[3]}</label>
+          
         </div>
+        <div className={classes.button}>
         <button type="submit">Next</button>
+        </div>
       </form>
     </Card>
   );
